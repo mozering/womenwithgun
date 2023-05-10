f94dce777570b3d48e0bf65836ed9ad40aa085c6
mozering <sallynz001@naver.com>
2023-05-10 11:44:14 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,84 @@
+const myElement1 = document.querySelector("#listimg1");
+const otherElement1 = document.querySelector("#listtext1");
+
+
+myElement1.addEventListener("mouseenter", function() {
+  // otherElement 클래스 추가 코드
+  listtext1.style.color = "#fa5887";
+  text1.style.color = "#fa5887";
+
+});
+
+myElement1.addEventListener("mouseleave", function () {
+  listtext1.style.color = "";
+  text1.style.color = "";
+
+});
+
+const myElement2 = document.querySelector("#listimg2");
+const otherElement2 = document.querySelector("#listtext2");
+
+
+myElement2.addEventListener("mouseenter", function() {
+  // otherElement 클래스 추가 코드
+  listtext2.style.color = "#fa5887";
+  text2.style.color = "#fa5887";
+
+});
+
+myElement2.addEventListener("mouseleave", function () {
+  listtext2.style.color = "";
+  text2.style.color = "";
+
+});
+
+const myElement3 = document.querySelector("#listimg3");
+const otherElement3 = document.querySelector("#listtext3");
+
+
+myElement3.addEventListener("mouseenter", function() {
+  // otherElement 클래스 추가 코드
+  listtext3.style.color = "#fa5887";
+  text3.style.color = "#fa5887";
+
+});
+
+myElement3.addEventListener("mouseleave", function () {
+  listtext3.style.color = "";
+  text3.style.color = "";
+
+});
+
+const myElement4_1 = document.querySelector("#listimg4-1");
+const otherElement4_1 = document.querySelector("#listtext4");
+
+
+myElement4_1.addEventListener("mouseenter", function() {
+  // otherElement 클래스 추가 코드
+  listtext4.style.color = "#fa5887";
+  text4.style.color = "#fa5887";
+
+});
+
+myElement4_1.addEventListener("mouseleave", function () {
+  listtext4.style.color = "";
+  text4.style.color = "";
+
+});
+
+const myElement4_2 = document.querySelector("#listimg4-2");
+const otherElement4_2 = document.querySelector("#listtext4");
+
+
+myElement4_2.addEventListener("mouseenter", function() {
+  // otherElement 클래스 추가 코드
+  listtext4.style.color = "#fa5887";
+  text4.style.color = "#fa5887";
+
+});
+
+myElement4_2.addEventListener("mouseleave", function () {
+  listtext4.style.color = "";
+  text4.style.color = "";
+
+});
